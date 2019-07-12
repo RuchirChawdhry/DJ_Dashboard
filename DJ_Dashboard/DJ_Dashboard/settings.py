6b263eb415680f42af7928b9cc2b63a4751bedb7
RuchirChawdhry <ruchirkc@gmail.com>
2019-07-12 12:52:04 +0530

    UI... amongst other small things

--- DJ_Dashboard/DJ_Dashboard/settings.py
+++ DJ_Dashboard/DJ_Dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jq6h_u_b%y57b%k-hjsg5m*x&&g@@0w8z%)s0&o@6fu$a!-9%^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'charts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DJ_Dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,''), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DJ_Dashboard/charts/views.py
+++ DJ_Dashboard/charts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
